2d7d727cff9bf8e181d56ee8d3904c1d8b5b1a7e
sonnyrus <sonny-69@live.fr>
2024-01-16 08:46:32 +0100

    colonnes faites mais manque de l'espacement

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,78 +1,75 @@
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
-        <link rel="stylesheet" href="{{ asset ('css/register.css')}}">
+<form method="POST" action="{{ route('register') }}">
+    @csrf
+    <link rel="stylesheet" href="{{ asset ('css/register.css')}}">
 
         <!-- Name -->
-        <img src="{{ asset('css/logo.png')}}" alt="Logo">
+    <img src="{{ asset('css/logo.png')}}" alt="Logo">
+    <div id="Donnees">
+        <div id="Donnees_personnelles">
+            <div class="mt-4">
+                <p class="lbl">Nom :</p>
+                <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" placeholder="Votre Nom"/>
+                <x-input-error :messages="$errors->get('name')" class="mt-2" />
+            </div>
 
-        <div class="Donnees_personnelles">
-        <div class="mt-4">
-            <p class="lbl">Nom :</p>
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" placeholder="Votre Nom"/>
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+            <!-- Surname -->
+            <div class="mt-4">
+                <p class="lbl">Prénom :</p>
+                <x-text-input id="firstname" class="block mt-1 w-full" type="text" name="firstname" :value="old('firstname')" required autofocus autocomplete="name" placeholder="Votre Prénom"/>
+                <x-input-error :messages="$errors->get('surname')" class="mt-2" />
+            </div>
+
+            <!-- Téléphone -->
+            <div class="mt-4">
+                <p class="lbl">Téléphone :</p>
+                <x-text-input id="name" class="block mt-1 w-full" type="text" name="telephone" :value="old('name')" required autofocus autocomplete="name" placeholder="Votre Téléphone"/>
+                <x-input-error :messages="$errors->get('name')" class="mt-2" />
+            </div>
+
+            <!-- Email Address -->
+            <div class="mt-4">
+                <p class="lbl">Email :</p>
+                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" placeholder="Votre Email" />
+                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            </div>
         </div>
+        <div id="Donnees_entreprises">
+            <!-- Nom Exploitation -->
+            <div class="mt-4">
+                <p class="lbl">Nom de l'exploitation:</p>
+                <x-text-input id="Exploitation_name" class="block mt-1 w-full" type="text" name="Exploitation_name" :value="old('Exploitation_name')" required autofocus autocomplete="name" placeholder="Votre Exploitation"/>
+                <x-input-error :messages="$errors->get('name')" class="mt-2" />
+            </div>
 
-        <!-- Surname -->
-        <div class="mt-4">
-            <p class="lbl">Prénom :</p>
-            <x-text-input id="firstname" class="block mt-1 w-full" type="text" name="firstname" :value="old('firstname')" required autofocus autocomplete="name" placeholder="Votre Prénom"/>
-            <x-input-error :messages="$errors->get('surname')" class="mt-2" />
+            <!-- N° de SIRET -->
+            <div class="mt-4">
+                <p class="lbl">Numéro de SIRET :</p>
+                <x-text-input id="name" class="block mt-1 w-full" type="text" name="SIRET" :value="old('SIRET')" required autofocus autocomplete="name" placeholder="Votre SIRET"/>
+                <x-input-error :messages="$errors->get('name')" class="mt-2" />
+            </div>
+
+            <!-- Password -->
+            <div class="mt-4">
+                <p class="lbl">Mot de passe :</p>
+                <x-text-input id="password" class="block mt-1 w-full"
+                                type="password"
+                                name="password"
+                                required autocomplete="new-password" 
+                                placeholder="Créer un Mot de passe"/>
+                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            </div>
+
+            <!-- Confirm Password -->
+            <div class="mt-4">
+                <p class="lbl">Confirmer le Mot de passe :</p>
+                <x-text-input id="password_confirmation" class="block mt-1 w-full"
+                                type="password"
+                                name="password_confirmation" required autocomplete="new-password"
+                                placeholder="Confirmer votre Mot de passe" />
+                <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+            </div>
         </div>
-
-         <!-- Téléphone -->
-         <div class="mt-4">
-            <p class="lbl">Téléphone :</p>
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="telephone" :value="old('name')" required autofocus autocomplete="name" placeholder="Votre Téléphone"/>
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <p class="lbl">Email :</p>
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" placeholder="Votre Email" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-</div>
-<div class="Donnees_entreprises">
-        <!-- Nom Exploitation -->
-        <div class="mt-4">
-            <p class="lbl">Nom de l'exploitation:</p>
-            <x-text-input id="Exploitation_name" class="block mt-1 w-full" type="text" name="Exploitation_name" :value="old('Exploitation_name')" required autofocus autocomplete="name" placeholder="Votre Exploitation"/>
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
-
-        <!-- N° de SIRET -->
-        <div class="mt-4">
-            <p class="lbl">Numéro de SIRET :</p>
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="SIRET" :value="old('SIRET')" required autofocus autocomplete="name" placeholder="Votre SIRET"/>
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <p class="lbl">Mot de passe :</p>
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" 
-                            placeholder="Créer un Mot de passe"/>
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <p class="lbl">Confirmer le Mot de passe :</p>
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password"
-                            placeholder="Confirmer votre Mot de passe" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-</div>
+    </div>
 
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
@@ -83,4 +80,4 @@
                 {{ __('Enregistrer') }}
             </x-primary-button>
         </div>
-    </form>
+</form>
