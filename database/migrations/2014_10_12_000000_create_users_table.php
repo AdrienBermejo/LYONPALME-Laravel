b43a2850f01cacab97c31c04b78eb57950420b04
sonnyrus <sonny-69@live.fr>
2024-01-16 13:49:17 +0100

    Migrations prêtes ne manque plus que le test pour voir si tout marche

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('firstname');
+            $table->string('telephone');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('Exploitation_name');
+            $table->string('SIRET');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
