aac33874056f3f135b1caa36a0bfa967d98d831e
sonnyrusso <sonnyrusso@leschassagnes.net>
2024-01-17 09:27:47 +0100

    Modification de la BDD

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('Exploitation_name');
             $table->string('SIRET');
             $table->string('password');
+            $table->boolean('is_admin')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
