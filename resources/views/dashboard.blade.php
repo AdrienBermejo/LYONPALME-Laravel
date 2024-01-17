d7761846fddc11dde8684d7ad04f7e35188c2c1f
sonnyrus <sonny-69@live.fr>
2024-01-17 09:02:54 +0100

    dasboard crée les valeurs sont à ajouter

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,39 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+<!DOCTYPE html>
+<head>
+    <title>La conserverie engagée de Lyon et du Rhône</title>
+    <link rel="stylesheet" href="{{ asset ('css/dashboard.css')}}">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/jquery.slick/1.6.0/slick.css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min.js"></script>
+</head>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
+<body>
+
+<section id="container">
+    <header>
+    <div class="logo-menu">
+        <div class="logo">
+            <img src="{{ asset('css/logo.png') }}" alt="Logo">
         </div>
+        <ul class="menu">
+            <li><a href="#">La Conserverie</a></li>
+            <li><a href="#produits">Les Produits</a></li>
+            <li><a href="#pointsvente">Les points de vente</a></li>
+            <li><a href="#journal">Journal Douceur</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
     </div>
-</x-app-layout>
+    <div class="auth">
+    @if(auth()->check())
+        <a href="{{ route ('dashboard') }}">{{ auth()->user()->firstname }} {{ auth()->user()->name }}</a>
+        <a href="{{ route('logout') }}">Logout</a>
+    @else
+        <a href="{{ route('login') }}">Sign In</a>
+        <a href="{{ route('register') }}">Sign Up</a>
+    @endif
+    </div>
+    </header>    
+<h1>
+        {{ __("Vous êtes connecté !") }}
+    </h1>
+                
