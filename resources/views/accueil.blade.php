897569c1ae550c323915ce36ca80d67077299f57
sonnyrus <sonnyrusso@leschassagnes.net>
2024-01-18 11:26:11 +0100

    Correction bug

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <title>La conserverie engagée de Lyon et du Rhône</title>
+    <title>Terra Douceurs</title>
     <link rel="stylesheet" href="{{ asset('css/accueil.css') }}">
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/jquery.slick/1.6.0/slick.css"/>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
 
 <body>
-
 <section id="container">
     <header>
     <div class="logo-menu">
@@ -26,11 +26,11 @@
     </div>
     <div class="auth">
     @if(auth()->check())
-        <span>{{ auth()->user()->first_name }} {{ auth()->user()->name }}</span>
-        <a href="{{ route('logout') }}">Logout</a>
+        <a>{{ auth()->user()->firstname }} {{ auth()->user()->name }}</a>
+        <a href="{{ route('logout') }}">Déconnexion</a>
     @else
-        <a href="{{ route('login') }}">Sign In</a>
-        <a href="{{ route('register') }}">Sign Up</a>
+        <a href="{{ route('login') }}">Connexion</a>
+        <a href="{{ route('register') }}">Inscription</a>
     @endif
     </div>
     </header>
@@ -39,7 +39,7 @@
             <p>La conserverie engagée<br>de Lyon et du Rhône !</p>
         </div>
         <div class="button-container">
-            <button class="button">Votre bouton</button>
+            <button class="button">Voir les produits</button>
         </div>
     </div>
 </section>
@@ -49,11 +49,16 @@
     @foreach($products as $product)
         <div class="product">
             <img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->title }}">
-            <h2>{{ $product->title }}</h2>
-            <p>{{ $product->is_bio ? 'Bio' : 'Non-bio' }}</p>
+            <div class="text-container">
+                <h2>{{ $product->title }}</h2>
+                <p>{{ $product->is_bio ? 'Bio' : 'Non-bio' }}</p>
+            </div>
         </div>
     @endforeach
 </div>
+
+<button class="slick-prev">Précédent</button>
+<button class="slick-next">Suivant</button>
 
 <script type="text/javascript">
     $(document).ready(function(){
@@ -63,6 +68,8 @@
             autoplay: true,
             autoplaySpeed: 2000,
             arrows: false,
+            prevArrow: $('.slick-prev'),
+            nextArrow: $('.slick-next'),
         });
     });
 </script>
