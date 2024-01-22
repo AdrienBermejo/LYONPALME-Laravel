ad4c961996147bee28e5f218bfc345ccb9c0b316
AdrienBermejo <adrienbermejogarcia@leschassagnes.net>
2024-01-22 14:30:42 +0100

    disponibilities qui marche normalement

--- database/migrations/2024_01_18_145011_create_disponibilites_table.php
+++ database/migrations/2024_01_18_145011_create_disponibilites_table.php
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('disponibilites');
+        Schema::dropIfExists('disponibilities');
     }
 };
