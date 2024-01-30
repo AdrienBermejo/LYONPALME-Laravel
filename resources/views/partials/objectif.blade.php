243ec82da3e0ab0eedf089171ffd396e83bfc60b
sonnyrusso <sonnyrusso@leschassagnes.net>
2024-01-30 08:35:40 +0100

    Correction de bugs

--- resources/views/partials/objectif.blade.php
+++ resources/views/partials/objectif.blade.php
@@ -9,13 +9,18 @@
             <ul>
                 <li>Vous gagnez du temps pour<br>gérer vos autres activités.</li>
                 <li>Vous répondez aux normes<br>sanitaires et d'étiquettage en<br>vigueur.</li>
-                <!-- Ajoutez les autres éléments de la liste -->
+                <li>Vous avez la garantie d'une<br>traçabilité complète de vos<br>produits transformés.</li>
+                <li>Vous diversifiez votre offre<br>produit.</li>
+                <li>Vous avez jusqu'à 3 ans pour<br>vendre votre production</li>
             </ul>
         </div>
         <div class="right-content">
             <h1>D'Autres Points</h1>
             <ul>
-                <!-- Ajoutez les éléments de la deuxième liste -->
+                <li>Vous disposez d'une offre complémentaire<br>pour vos circuits courts par lot d'environ<br>70kg de produits bruts (selon recette et<br>conditionnement).</li>
+                <li>Vous limitez le gaspillage de votre production<br>agricole.</li>
+                <li>Vous limitez l'empreinte carbone en favorisant<br>les circuits courts.</li>
+                <li>Vous favorisez l'emploi de personnes porteuses<br>de handicaps cognitifs</li>
             </ul>
         </div>
     </div>
