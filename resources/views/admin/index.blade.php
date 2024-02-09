188650eac35052482e912f602df437d1540524ed
Adrien Bermejo <adrienbermejogarcia@leschassagnes.net>
2024-02-09 08:26:42 +0100

    J'ai mis une croix pour supprimer le pop-up

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,10 +4,16 @@
     <!-- Inclusion du CSS pour la page d'administration -->
     <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
 </head>
-<div class="container mt-4 ">
+<div class="container mt-4">
     @if(session('success'))
-    <div class="rounded-lg w-1/4 mx-auto text-white bg-vert p-5 ">
+    <div class="rounded-lg w-1/4 mx-auto text-white bg-vert p-5 relative">
         <p>{{ session('success') }}</p>
+        <button class="absolute top-0 right-0 p-2" onclick="this.parentElement.style.display = 'none'">
+            <svg class="fill-current h-4 w-4 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <title>Close</title>
+                <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/>
+            </svg>
+        </button>
     </div>
     @endif
     <div class="row">
