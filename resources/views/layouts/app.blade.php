fb62f363ad679f71b72903fe0e061afbf5a52992
Adrien Bermejo <adrienbermejogarcia@leschassagnes.net>
2024-02-15 16:36:04 +0100

    Je suis en train de travailler la page des administrateurs réservation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,8 +16,8 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-[url('../../public/css/img/vegetablesbackground.jpg')] bg-cover bg-fixed">
+    <body class="font-sans antialiased min-h-screen bg-[url('../../public/css/img/vegetablesbackground.jpg')] bg-cover bg-fixed bg-repeat">
+        <div class="min-h-screen">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
