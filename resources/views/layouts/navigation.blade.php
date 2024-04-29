aa92c1f69ec235cd9b1b0ab0972cd965ca9e83b8
maximedemagalhaes <maximedemagalhaes@leschassagnes.net>
2024-04-29 11:58:29 +0200

    mise en forme du site avec les couleurs et images

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,6 +1,6 @@
 <nav x-data="{ open: false }" class="pt-4">
     <!-- Primary Navigation Menu -->
-    <div class="rounded-full bg-white border-b border-gray-100 max-w-7xl mx-auto px-4 sm:px-6 lg:px-7">
+    <div class="rounded-full bg-purple-700 border-b border-  -100 max-w-7xl mx-auto px-4 sm:px-6 lg:px-7">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
@@ -85,7 +85,7 @@
 
             <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-blue-400 hover:text-blue-500 hover:bg-blue-100 focus:outline-none focus:bg-blue-100 focus:text-blue-500 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                         <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
@@ -96,7 +96,7 @@
     </div>
 
     <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden bg-white border-b border-gray-100 rounded-lg">
+    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden bg-white border-b border-blue-400 rounded-lg">
         <div class="pt-2 pb-3 space-y-1">
         
             <x-responsive-nav-link class="text-framboise" :href="route('appointements.index')" :active="request()->routeIs('/appointements')">
@@ -125,7 +125,7 @@
         @endif
 
         <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200">
+        <div class="pt-4 pb-1 border-t border-blue-400">
             <div class="px-4">
                 <div class="font-medium text-base text-framboisehover">{{ Auth::user()->name }}</div>
                 <div class="font-medium text-sm text-framboise">{{ Auth::user()->email }}</div>
